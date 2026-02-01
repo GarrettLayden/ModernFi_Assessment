15046371d5698f378371216d186eef675ce9a708
Garrett Layden <garlayden@gmail.com>
2026-02-01 12:50:47 -0500

    Routes orders html and endpoint to server

--- assessment/server.js
+++ assessment/server.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
+// Routing HTML
+app.use(express.static(__dirname + '/'));
+
+app.get('/orders', (req, res) => {
+    res.sendFile(__dirname + '/Orders.html');
+});
+
+// Starting APIs
+const orders = require('./routes/orders');
+app.use('/orders', orders);
+
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/404.html');
 });
